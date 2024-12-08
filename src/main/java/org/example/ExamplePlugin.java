3c329807787d71437bfea4643718442dca17783f
pawpl <pawpl94w91@gmail.com>
2024-12-08 17:37:17 -0500

    Update ExamplePlugin.java

--- src/main/java/org/example/ExamplePlugin.java
+++ src/main/java/org/example/ExamplePlugin.java
@@ -1,37 +1,40 @@
-package org.example;
+package org.pawpl;
 
 import org.rusherhack.client.api.RusherHackAPI;
 import org.rusherhack.client.api.plugin.Plugin;
+import org.rusherhack.client.api.utils.ChatUtils;
+import org.rusherhack.client.api.feature.module.Module;
+import org.rusherhack.client.api.feature.module.ModuleCategory;
 
 /**
- * Example rusherhack plugin
- *
- * @author John200410
+ * RusherHack plugin that registers the AutoHoldWAndSpace module, a HUD element, and a command.
  */
-public class ExamplePlugin extends Plugin {
-	
-	@Override
-	public void onLoad() {
-		
-		//logger
-		this.getLogger().info("Hello World!");
-		
-		//creating and registering a new module
-		final ExampleModule exampleModule = new ExampleModule();
-		RusherHackAPI.getModuleManager().registerFeature(exampleModule);
-		
-		//creating and registering a new hud element
-		final ExampleHudElement exampleHudElement = new ExampleHudElement();
-		RusherHackAPI.getHudManager().registerFeature(exampleHudElement);
-		
-		//creating and registering a new command
-		final ExampleCommand exampleCommand = new ExampleCommand();
-		RusherHackAPI.getCommandManager().registerFeature(exampleCommand);
-	}
-	
-	@Override
-	public void onUnload() {
-		this.getLogger().info("Example plugin unloaded!");
-	}
-	
+public class AutoHoldWAndSpacePlugin extends Plugin {
+
+    @Override
+    public void onLoad() {
+        // Log a message when the plugin is loaded
+        this.getLogger().info("AutoHoldWAndSpace plugin loaded!");
+
+        // Create and register the AutoHoldWAndSpace module
+        final AutoHoldWAndSpaceModule autoHoldModule = new AutoHoldWAndSpaceModule();
+        RusherHackAPI.getModuleManager().registerFeature(autoHoldModule);
+        
+        // Create and register a HUD element to display module status
+        final AutoHoldWAndSpaceHudElement hudElement = new AutoHoldWAndSpaceHudElement();
+        RusherHackAPI.getHudManager().registerFeature(hudElement);
+        
+        // Create and register a command to toggle the module
+        final AutoHoldWAndSpaceCommand toggleCommand = new AutoHoldWAndSpaceCommand();
+        RusherHackAPI.getCommandManager().registerFeature(toggleCommand);
+
+        // Inform the user that the plugin has loaded
+        ChatUtils.print("AutoHoldWAndSpace plugin is active!");
+    }
+
+    @Override
+    public void onUnload() {
+        // Log a message when the plugin is unloaded
+        this.getLogger().info("AutoHoldWAndSpace plugin unloaded!");
+    }
 }
